5e98002842a92acda820c6331942ca23592d3fbd
Elle Mundy <exchgr@icloud.com>
2025-09-21 01:17:29 -0400

    Render empty feed
    Also set up editorconfig

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,17 @@ app.use((req, _res, next) => {
 })
 
 app.get("/feed.xml", (req, res) => {
+	const updatedAt = new Date()
+	updatedAt.setHours(0, 0, 0, 0)
+
 	res.setHeader("Content-Type", "application/rss+xml")
-	res.render("feed.xml.hbs")
+	res.render(
+		"feed.xml.hbs",
+		{
+			updatedAt: updatedAt.toISOString(),
+			hostname: "http://localhost:3000",
+		},
+	)
 })
 
 app.listen(port, () => {
--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -11,7 +11,22 @@ describe("app", () => {
 		it("should render", async () => {
 			const response = await requester.get("/feed.xml")
 
-			expect(response.body.toString()).to.eq("hello world!\n")
+			const date = new Date()
+			date.setHours(0, 0, 0, 0)
+
+			// TODO: change link rel="self"
+			expect(response.body.toString()).to.eq(`<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom"
+      xml:base="https://en.wikipedia.org/wiki/Portal:Current_events">
+  <title>Wikipedia Current Events</title>
+  <subtitle>From Wikipedia, the free encyclopedia</subtitle>
+  <link href="http://localhost:3000/feed.xml" rel="self"/>
+  <link href="https://en.wikipedia.org/wiki/Portal:Current_events"/>
+  <updated>${date.toISOString()}</updated>
+  <id>https://en.wikipedia.org/wiki/Portal:Current_events</id>
+  <entry></entry>
+</feed>
+`)
 			expect(response).to.have.status(200)
 			expect(response).to.have.header(
 				"Content-Type",
