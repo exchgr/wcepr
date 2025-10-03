3d6c3f169f72550c03eb2384483546002d32ca2b
Elle Mundy <exchgr@icloud.com>
2025-10-03 17:00:22 -0400

    Use AAAA record instead of CNAME, which doesn't work with fly

--- terraform/cloudflare_record.tf
+++ terraform/cloudflare_record.tf
@@ -2,8 +2,8 @@ resource "cloudflare_dns_record" "wcepr" {
   name    = "wcepr"
   proxied = true
   ttl     = 1
-  type    = "CNAME"
-  content = data.external.env.result["WCEPR_CNAME"]
+  type    = "AAAA"
+  content = data.external.env.result["WCEPR_AAAA"]
   zone_id = data.external.env.result["CLOUDFLARE_ZONE_ID"]
 }
 
